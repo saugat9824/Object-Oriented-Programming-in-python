e52685a3bddf890c0de41755aee7d347af0c35c2
Saugat <saugatrijal99@gmail.com>
2024-06-06 10:43:28 +0545

    Solution 2

---
+++ 02_Soln.py
@@ -0,0 +1,17 @@
+# Class Methode and Self
+# Add a method to the Car class thaht displays the full name of the car ( brand and model).
+
+
+class Car:
+    def __init__(self, brand, model): 
+        self.brand = brand
+        self.model = model
+
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+
+my_car = Car("Toyota", "Corolla") # object 
+print(my_car.brand)
+print(my_car.model)
+print(my_car.full_name())
